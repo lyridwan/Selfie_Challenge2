1b0da2e8a797ee2630c7da9e967e47a501f366be
showbizlyra <just.muhammadridwan@gmail.com>
2016-12-12 19:53:46 +0700

    Add exit confirmation, in menu activity

--- app/src/main/java/showbizlyra/tumblr/com/gmaps/MenuActivity.java
+++ app/src/main/java/showbizlyra/tumblr/com/gmaps/MenuActivity.java
@@ -1,7 +1,9 @@
 package showbizlyra.tumblr.com.gmaps;
 
 import android.animation.ObjectAnimator;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -55,5 +57,24 @@ public class MenuActivity extends AppCompatActivity {
     public void onClick(View v){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
